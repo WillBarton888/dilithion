45450d81264e63ac6843afc28cd423590c38300b
WillBarton888 <will@bananatree.com.au>
2025-11-15 18:24:57 +1030

    fix: Forward declare NodeState in global stubs

--- src/util/global_stubs.cpp
+++ src/util/global_stubs.cpp
@@ -5,6 +5,9 @@
 #include "node/blockchain_storage.h"
 #include "consensus/chain.h"
 
+// Forward declare NodeState (defined in rpc/server.cpp)
+struct NodeState;
+
 // Stub definitions to satisfy linker
 CBlockchainDB* g_blockchain = nullptr;
 CChainState* g_chainstate = nullptr;
