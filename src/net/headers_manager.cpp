21390f689d43f2ef0008b5df7925fb677933b915
WillBarton888 <will@bananatree.com.au>
2026-01-03 19:03:19 +1030

    debug: Show end of chainWork hex to see actual values

--- src/net/headers_manager.cpp
+++ src/net/headers_manager.cpp
@@ -1286,10 +1286,13 @@ bool CHeadersManager::UpdateBestHeader(const uint256& hash)
     // IBD DEBUG: Log every 100th comparison or when update happens
     static int compare_count = 0;
     if (hasMoreWork || (compare_count++ % 100 == 0)) {
+        // Show LAST 16 chars of hex (where non-zero work resides for easy targets)
+        std::string currWork = bestIt->second.chainWork.GetHex();
+        std::string newWork = it->second.chainWork.GetHex();
         std::cout << "[UpdateBestHeader] height=" << nBestHeight << " vs new=" << newHeight
                   << " hasMoreWork=" << hasMoreWork
-                  << " current_work=" << bestIt->second.chainWork.GetHex().substr(0, 16)
-                  << " new_work=" << it->second.chainWork.GetHex().substr(0, 16) << std::endl;
+                  << " current_work=..." << currWork.substr(currWork.size() > 16 ? currWork.size() - 16 : 0)
+                  << " new_work=..." << newWork.substr(newWork.size() > 16 ? newWork.size() - 16 : 0) << std::endl;
     }
 
     if (hasMoreWork) {
